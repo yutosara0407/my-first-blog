9b1ffa8440cbc1be99b78ca11bf81da20c8af0d8
Yuto <yutosara0407@gmail.com>
2021-04-28 20:41:35 +0900

    Third commit. Test pillow.

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Member, Circle, Gymnasium
+from .models import Member, Circle, Gymnasium, MemberImage
 
 admin.site.register(Member)
 admin.site.register(Circle)
 admin.site.register(Gymnasium)
+admin.site.register(MemberImage)
