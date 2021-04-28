4960f37f998847e4b267bc218c302708555b1680
Yuto <yutosara0407@gmail.com>
2021-04-29 00:57:00 +0900

    5th commit change setting

--- delight/urls.py
+++ delight/urls.py
@@ -12,7 +12,5 @@ urlpatterns = [
 
 # 開発環境時のみ、Djangoアプリケーション側でメディアファイルを配信する
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- myapp/forms.py
+++ myapp/forms.py
@@ -2,18 +2,21 @@ from django import forms
 from .models import Member, Circle, MemberImage
 from PIL import Image
 
-
+"""
 class SearchForm(forms.Form):
     member = forms.ModelChoiceField(
         queryset=Member.objects, label='メンバー', required=False)
 
     circle = forms.ModelChoiceField(
         queryset=Circle.objects, label='サークル', required=False) 
+"""
 
-class MemberImageForm(forms.ModelForm):
+class MemberImageForm(forms.Form):
     class Meta:
         model = MemberImage
         fields = ('member_image',)
 
+"""
 class UpLoadMemberImageForm(forms.Form):
     member_image = forms.ImageField(required=True)
+"""
